262f61f4a90b530085683b322d5fea7713ab01df
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2023-06-21 19:13:58 +0530

    Add files via upload
    change

---
+++ index.js
@@ -0,0 +1,15 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Learner",
+      "Web Developer",
+      "Consisten Learner",
+      "Front-End Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
+  
+
